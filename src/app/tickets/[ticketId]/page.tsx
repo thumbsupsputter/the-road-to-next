5020499e83b1fa94d556696aa00b37146cdb74e5
thumbsupsputter <loanpeachervfo87@gmail.com>
2024-11-26 09:39:25 +0800

    Tickets Page

---
+++ src/app/tickets/[ticketId]/page.tsx
@@ -0,0 +1,10 @@
+interface TicketPageProps {
+  params: Promise<{ ticketId: string }>;
+}
+
+const TicketPage = async ({ params }: TicketPageProps) => {
+  const { ticketId } = await params;
+  return <h2 className="text-lg">TicketPage: {ticketId}</h2>;
+};
+
+export default TicketPage;
---
+++ src/app/tickets/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const TicketsPage = () => {
+  return (
+    <div>
+      <Link href="/" className="underline">
+        Go to Home
+      </Link>
+      <h1>Hello Tickets Page</h1>
+    </div>
+  );
+};
+export default TicketsPage;
